33a6bde683aa6c412c2a3e6df471e3f7e62d76c4
vkook-ot7 <sumithra96523@gmail.com>
2026-01-31 15:35:57 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,16 @@
+
+class Max
+{
+    public static void main(String args[])
+    {
+       int a=12;
+       int b=23;
+       int c=7;
+       if(a>b&&a>c)
+       System.out.println(" A IS MAX");
+       if(b>a&&b>c)
+       System.out.println("B IS MAX");
+       else
+        System.out.println("C IS MAX");
+    }
+}

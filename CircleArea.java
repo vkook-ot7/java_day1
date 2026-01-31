87795195532a751674b976118c6bfd7347bef05d
vkook-ot7 <sumithra96523@gmail.com>
2026-01-31 14:31:23 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,10 @@
+class CircleArea
+{
+    public static void main(String args[])
+    {
+        double r=5;
+        double area;
+        area=r*r*3.14;
+        System.out.println(area);
+    }
+}

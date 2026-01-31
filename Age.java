6f564d8ff71200bb9f47b267f08a7c486b24b87a
vkook-ot7 <sumithra96523@gmail.com>
2026-01-31 15:02:15 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Age
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        System.out.println(age);
+    }
+}
